cfc35678a359efcdd9c3f4ad5b7c23b838b5b59c
Jhoy Rodrigues <jhoymartins@gmail.com>
2023-04-06 17:05:30 -0300

    1.001

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
       <Contact />
-    </div>
   );
 }
 export default App;
--- src/Contact.js
+++ src/Contact.js
@@ -6,10 +6,10 @@ import logoPhonebook from './assets/phonebook.png'
 
 function Contact() {
 
-    const [updateData, setUpdateData] = useState(true);
-    const [contacts, setContacts] = useState([]);
     const baseURL = "https://localhost:7094/Contact";
+    const [updateData, setUpdateData] = useState(true);
     const [data, setData] = useState([]);
+    const [contacts, setContacts] = useState(data);
 
     const getOrder = async () => {
         await axios.get(baseURL)
@@ -21,12 +21,12 @@ function Contact() {
     }
 
     function sortBy(field) {
-        const sortedContacts = [...contacts].sort((a, b) => a[field] > b[field] ? 1 : 1);
+        const sortedContacts = Array.from(contacts).sort((a, b) => a[field] < b[field] ? -1 : 1);
         setContacts(sortedContacts);
     }
 
     function groupBy(field) {
-        const groupedContacts = [...contacts].reduce((groups, contact) => {
+        const groupedContacts = [...data].reduce((groups, contact) => {
             const group = contact[field];
             if (!groups[group]) {
                 groups[group] = [];
@@ -42,7 +42,23 @@ function Contact() {
             getOrder();
             setUpdateData(false);
         }
-    });
+    }, [contacts, updateData]);
+
+    function ContactItem(props) {
+        const { id, name, branch, tellPhone, cellPhone, email, sector } = props;
+        return (
+            <tr>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{branch}</td>
+                <td>{tellPhone}</td>
+                <td>{cellPhone}</td>
+                <td>{email}</td>
+                <td>{sector}</td>
+            </tr>
+        );
+    }
+
 
     return (
         <div className="user-container">
@@ -51,17 +67,17 @@ function Contact() {
                 <img src={logoPhonebook} alt='Phonebook'></img>
                 <h1>Agenda de colaboradores</h1>
             </header>
-            <div class="btn-sort">
+            <div className="btn-sort">
                 <button onClick={() => sortBy('name')}>Ordenar por Nome</button>
                 <button onClick={() => sortBy('sector')}>Ordenar por Setor</button>
                 <button onClick={() => groupBy('sector')}>Agrupar por Setor</button>
                 {Array.isArray(contacts) ? (
-                    contacts.map(contact => <contact key={contact.id} {...contact} />)
+                    contacts.map(contact => <ContactItem key={contact.id} {...contact} />)
                 ) : (
                     Object.keys(contacts).map(group => (
                         <div key={group}>
                             <h2>{group}</h2>
-                            {contacts[group].map(contact => <contact key={contact.id} {...contact} />)}
+                            {Array.isArray(contacts[group]) ? contacts[group].map(contact => <ContactItem key={contact.id} {...contact} />) : null}
                         </div>
                     ))
                 )}
@@ -76,12 +92,12 @@ function Contact() {
                         <th>Celular</th>
                         <th>Email</th>
                         <th>Setor</th>
-                        <th>Operação</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className='dados'>
                     {data.map(user => (
-                        <tr key={user.id}>
+                        // <tr key={user.id}>
+                        <tr>
                             <th>{user.id}</th>
                             <th>{user.name}</th>
                             <th>{user.branch}</th>
@@ -89,13 +105,11 @@ function Contact() {
                             <th>{user.cellPhone}</th>
                             <th>{user.email}</th>
                             <th>{user.sector}</th>
-                            <td>
-                            </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-        </div>
+        </div> //user-container
     );
 }
 
